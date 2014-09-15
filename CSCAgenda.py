1a589385d6919ce7d1f8b982b33c7fca92c50b45
WillEngler <whe1@pitt.edu>
2014-09-15 15:53:50 -0400

    Enable claiming single role with checkbox

--- CSCAgenda.py
+++ CSCAgenda.py
@@ -23,14 +23,20 @@ class Role(ndb.Model):
     description = ndb.StringProperty()
     presenter = ndb.StringProperty()
     minutes = ndb.IntegerProperty()
+    title = ndb.StringProperty()
     notes = ndb.StringProperty()
+    isClaimed = ndb.BooleanProperty()
     
     #Grab every role associated with a meeting
     @staticmethod
-    def getAllRoles(agendaName):
-        key = ndb.Key('Agenda', agendaName)
-        roles_query = Role.query(ancestor=Agenda.gen_key(agendaName))
-        return roles_query.fetch()
+    def getAllRoles(agenda_name):
+        return Role.query(ancestor=Agenda.gen_key(agenda_name))
+
+    #Given names for an agenda and a role, generate a key for an agenda
+    @staticmethod
+    def gen_key(agenda_name,role_name):
+        import pdb; pdb.set_trace();
+        return ndb.Key('Agenda',agenda_name,'Role',role_name)
 
 class Agenda(ndb.Model):
     """Models an agenda for one meeting."""        
@@ -39,13 +45,20 @@ class Agenda(ndb.Model):
 
     # Create a speaker role and attach it to this agenda
     def addDefaultSpeaker(self):
-        speaker = Role(parent=self.gen_key(self.name), description=roleinfo.speakerDescription, notes = roleinfo.speakerNotes, minutes = roleinfo.speakerMinutes, presenter = roleinfo.presenterName)
+        speaker = Role(parent=Agenda.gen_key(self.name), 
+        description=roleinfo.speakerDescription, 
+        notes = roleinfo.speakerNotes, 
+        minutes = roleinfo.speakerMinutes, 
+        presenter = roleinfo.presenterName, 
+        isClaimed = False,
+        title = "Speaker")
+        
         speaker.put()
 
     #Given a name, generate a key for an agenda
     @staticmethod
-    def gen_key(agendaName):
-        return ndb.Key('Agenda',agendaName)
+    def gen_key(agenda_name):
+        return ndb.Key('Agenda',agenda_name)
 
     #Grab all coming agendas in order from soonest to farthest in the future
     @staticmethod
@@ -57,6 +70,18 @@ class Agenda(ndb.Model):
     def getAll():
         agendas_query =  Agenda.getQuery()
         return agendas_query.fetch()
+
+def renderAgenda(agenda_name,handler):
+    #Grab all of the roles associated with this agenda
+    roles = Role.getAllRoles(agenda_name)
+
+    #Prepare data for JINJA templating
+    template_values = {'roles': roles, 'agenda_name': agenda_name}
+
+    #Render the page
+    template = JINJA_ENVIRONMENT.get_template('agenda.html')
+    handler.response.write(template.render(template_values))
+
     
 class MainPage(webapp2.RequestHandler):
 
@@ -85,43 +110,59 @@ class NewAgenda(webapp2.RequestHandler):
         #If an agenda already exists on the requested date, reject creation
         dateAsListOfInts = [int(x) for x in self.request.get('meeting').split('-')]
         date = datetime.date(*dateAsListOfInts)
-        name = "{0} {1}, {2}".format(calendar.month_name[dateAsListOfInts[1]], dateAsListOfInts[2], dateAsListOfInts[0])
+        agenda_name = "{0} {1}, {2}".format(calendar.month_name[dateAsListOfInts[1]], dateAsListOfInts[2], dateAsListOfInts[0])
 
         #import pdb; pdb.set_trace();
         #If an agenda already exists on the requested date, reject creation
         agendaQuery = Agenda.getQuery()
         for agenda in agendaQuery:
-            if agenda.name == name:
+            if agenda.name == agenda_name:
                 query_params = {'errorMessage': "A meeting already exists on that day."}
                 self.redirect('/?' + urllib.urlencode(query_params))
                 return
 
         #Create, initialize, and store new agenda
-        newAgenda = Agenda(date=date,name=name)
+        newAgenda = Agenda(key = Agenda.gen_key(agenda_name),date=date,name=agenda_name)
         newAgenda.addDefaultSpeaker()
         newAgenda.put()
 
-        #Redirect to the newly created agenda
-        query_params = {'agenda_name': name}
-        self.redirect('/displayagenda?' + urllib.urlencode(query_params))
+        renderAgenda(agenda_name,self)
 
 
 class DisplayAgenda(webapp2.RequestHandler):
 
     def get(self):
         #Grab all of the roles associated with this agenda
-        agendaName = self.request.get('agenda_name')
-        roles = Role.getAllRoles(agendaName)
+        agenda_name = self.request.get('agenda_name')
+        renderAgenda(agenda_name,self)
 
-        #Prepare data for JINJA templating
-        template_values = {'roles': roles, 'agenda_name': agendaName}
 
-        #Render the page
-        template = JINJA_ENVIRONMENT.get_template('agenda.html')
-        self.response.write(template.render(template_values))
+class EditAgenda(webapp2.RequestHandler):
+
+    def post(self):
+        #Grab the role we want to edit
+        agenda_name = self.request.get('agenda_name')
+        role_being_edited = self.request.get('title')
+        query = Role.getAllRoles(agenda_name).filter(Role.title == role_being_edited)
+
+        #Temporary janky loop. Should be identifying one unique role
+        for role in query:
+            #Edit the role
+            claimed = self.request.get('isClaimed')
+            if claimed == 'on':
+                role.isClaimed = True
+            else:
+                role.isClaimed = False
+            #Store the edits
+            role.put()
+
+        #Show the edited agenda
+        renderAgenda(agenda_name, self)
+
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/newagenda', NewAgenda ),
-    ('/displayagenda', DisplayAgenda)
+    ('/displayagenda', DisplayAgenda),
+    ('/edit', EditAgenda)
 ], debug=True)
