01dff69b23771f1a48593c12f55bf67df7430c86
WillEngler <whe1@pitt.edu>
2014-09-15 09:58:10 -0400

    Prevent duplicate meetings. Comment code

--- CSCAgenda.py
+++ CSCAgenda.py
@@ -10,6 +10,8 @@ from google.appengine.ext import ndb
 import jinja2
 import webapp2
 
+#If you want to debug, uncomment the line below and stick it wherever you want to break
+#import pdb; pdb.set_trace();
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -23,31 +25,56 @@ class Role(ndb.Model):
     minutes = ndb.IntegerProperty()
     notes = ndb.StringProperty()
     
+    #Grab every role associated with a meeting
+    @staticmethod
+    def getAllRoles(agendaName):
+        key = ndb.Key('Agenda', agendaName)
+        roles_query = Role.query(ancestor=Agenda.gen_key(agendaName))
+        return roles_query.fetch()
+
 class Agenda(ndb.Model):
     """Models an agenda for one meeting."""        
     date = ndb.DateProperty()
     name = ndb.StringProperty()
 
+    # Create a speaker role and attach it to this agenda
     def addDefaultSpeaker(self):
         speaker = Role(parent=self.gen_key(self.name), description=roleinfo.speakerDescription, notes = roleinfo.speakerNotes, minutes = roleinfo.speakerMinutes, presenter = roleinfo.presenterName)
         speaker.put()
 
+    #Given a name, generate a key for an agenda
     @staticmethod
     def gen_key(agendaName):
         return ndb.Key('Agenda',agendaName)
+
+    #Grab all coming agendas in order from soonest to farthest in the future
+    @staticmethod
+    def getQuery():
+        return Agenda.query().order(-Agenda.date)
+
+    #Return a list of every agenda
+    @staticmethod
+    def getAll():
+        agendas_query =  Agenda.getQuery()
+        return agendas_query.fetch()
     
 class MainPage(webapp2.RequestHandler):
 
     def get(self):
        
-        #Grab all coming agendas in order from soonest to farthest in the future
-        agendas_query = Agenda.query().order(-Agenda.date) # > datetime.datetime.today)
-        agendas = agendas_query.fetch()
+        #See if there is an error to display
+        msg = self.request.get('errorMessage')
 
+        #Grab a query with every agenda
+        agendas = Agenda.getAll()
+
+        #Prepare our data for JINJA templating
         template_values = {
             'agendas': agendas,
+            'errorMessage': msg,
         }
 
+        #Render the page
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(template_values))
 
@@ -55,15 +82,26 @@ class NewAgenda(webapp2.RequestHandler):
 
     def post(self):
 
+        #If an agenda already exists on the requested date, reject creation
         dateAsListOfInts = [int(x) for x in self.request.get('meeting').split('-')]
         date = datetime.date(*dateAsListOfInts)
-        
         name = "{0} {1}, {2}".format(calendar.month_name[dateAsListOfInts[1]], dateAsListOfInts[2], dateAsListOfInts[0])
 
-        agenda = Agenda(date=date,name=name)
-        agenda.addDefaultSpeaker()
-        agenda.put()
+        #import pdb; pdb.set_trace();
+        #If an agenda already exists on the requested date, reject creation
+        agendaQuery = Agenda.getQuery()
+        for agenda in agendaQuery:
+            if agenda.name == name:
+                query_params = {'errorMessage': "A meeting already exists on that day."}
+                self.redirect('/?' + urllib.urlencode(query_params))
+                return
 
+        #Create, initialize, and store new agenda
+        newAgenda = Agenda(date=date,name=name)
+        newAgenda.addDefaultSpeaker()
+        newAgenda.put()
+
+        #Redirect to the newly created agenda
         query_params = {'agenda_name': name}
         self.redirect('/displayagenda?' + urllib.urlencode(query_params))
 
@@ -71,18 +109,14 @@ class NewAgenda(webapp2.RequestHandler):
 class DisplayAgenda(webapp2.RequestHandler):
 
     def get(self):
+        #Grab all of the roles associated with this agenda
         agendaName = self.request.get('agenda_name')
-        key = ndb.Key('Agenda', agendaName)
+        roles = Role.getAllRoles(agendaName)
 
-        #import pdb; pdb.set_trace();
-
-        roles_query = Role.query(
-            ancestor=Agenda.gen_key(agendaName))
-
-        roles = roles_query.fetch(15)
-
+        #Prepare data for JINJA templating
         template_values = {'roles': roles, 'agenda_name': agendaName}
 
+        #Render the page
         template = JINJA_ENVIRONMENT.get_template('agenda.html')
         self.response.write(template.render(template_values))
 
